1355283ef66bada0f1ba6d18eb7655e2ebdd252f
Chris Kerbo <chris.kerbo@outlook.com>
2020-02-05 22:16:50 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,20 @@
 class SessionsController < ApplicationController
 
+    def new
+          # nothing to do here!
+      end
 
+      def create
+        if params[:username] != nil && params[:username].present?
+          session[:username] = params[:username]
+          redirect_to '/'
+        else
+          redirect_to '/login'
+        end
+      end
+
+      def destroy
+        session.delete :username
+      end
 
 end
