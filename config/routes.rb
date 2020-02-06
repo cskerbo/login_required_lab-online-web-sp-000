8439e2464cd7ababbad8eb3767ab5a21a1ea419d
Chris Kerbo <chris.kerbo@outlook.com>
2020-02-06 00:23:07 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/show' => 'sessions#show'
   post '/create' => 'sessions#create'
-  post '/new' => 'sessions#new'
+  get '/new' => 'sessions#new'
   get '/home' => 'sessions#home'
-  get '/destroy' => 'secrets#destroy'
+  post '/destroy' => 'secrets#destroy'
 end
